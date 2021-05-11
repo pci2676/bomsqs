ef65a90aa2c5f7bd0c39e1e05e8c83805282a607
chanin <pci2676@gmail.com>
2021-05-11 22:22:39 +0900

    project init

---
+++ src/main/kotlin/com/javabom/bomsqs/BomsqsApplication.kt
@@ -0,0 +1,11 @@
+package com.javabom.bomsqs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BomsqsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BomsqsApplication>(*args)
+}
---
+++ src/test/kotlin/com/javabom/bomsqs/BomsqsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.javabom.bomsqs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BomsqsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
